1a2dd5ac0f2df8b4a573bf2f97dfb515029c34e3
supramaxis <88255405+supramaxis@users.noreply.github.com>
2024-05-17 20:43:32 -0600

    Update installZsh.sh

--- customization/installZsh.sh
+++ customization/installZsh.sh
@@ -6,17 +6,22 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+# Define color codes
+# RED='\033[0;31m'
+GREEN='\033[0;32m'
+# YELLOW='\033[0;33m'
+RESET='\033[0m'
+
+function log() {
+  echo -e "[init] $*"
+}
+
 # Install Zsh if it's not already installed
 if ! [ -x "$(command -v zsh)" ]; then
   echo "Installing Zsh..."
   apt update
   apt install -y zsh
 fi
-
-
-
-# Create a backup directory for existing Zsh configuration files
-mkdir -p ~/zsh-backup
 
 # Loop through the Zsh configuration files and backup or remove them
 for rc in ~/.zshenv ~/.zprofile ~/.zshrc ~/.zlogin ~/.zlogout ~/.oh-my-zsh; do
@@ -30,12 +35,76 @@ for rc in ~/.zshenv ~/.zprofile ~/.zshrc ~/.zlogin ~/.zlogout ~/.oh-my-zsh; do
   fi
 done
 
-# Clone Oh My Zsh and Powerlevel10k themes
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+# Function to check if a package is installed
+function is_installed() {
+  dpkg-query -W -f='${Status}' "$1" 2>/dev/null | grep -c "ok installed"
+}
 
-# Configure Zsh to use Powerlevel10k theme
-sed 's|robbyrussell|powerlevel10k/powerlevel10k|' ~/.oh-my-zsh/templates/zshrc.zsh-template > ~/.zshrc
+# Install fontconfig and unzip if they are not already installed
+if [ "$(is_installed fontconfig)" -eq 0 ]; then
+  log "${GREEN}Installing fontconfig${RESET}"
+  apt install fontconfig -y
+else
+  log "${GREEN}fontconfig is already installed${RESET}"
+fi
 
-# Set ZDOTDIR and start a new Zsh session
+if [ "$(is_installed unzip)" -eq 0 ]; then
+  log "${GREEN}Installing unzip${RESET}"
+  apt install unzip -y
+else
+  log "${GREEN}unzip is already installed${RESET}"
+fi
+
+# Check if fonts are already installed
+if ls /usr/share/fonts/*.ttf 1> /dev/null 2>&1; then
+  log "${GREEN}Fonts are already installed, skipping download.${RESET}"
+else
+  mkdir cascadiacode && cd cascadiacode || exit
+
+  log "${GREEN}Installing CascadiaCode font${RESET}"
+
+  wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaCode.zip
+
+  unzip CascadiaCode.zip
+
+  sudo mv ./*.ttf /usr/share/fonts
+  log "${GREEN}Moved downloaded fonts to /usr/share/fonts${RESET}"
+
+  fc-cache -fv
+
+  cd ..
+
+  log "${GREEN}Removed workdir cascadiacode${RESET}"
+  rm -rf cascadiacode
+fi
+
+sudo -u dmt bash << 'EOF'
+curl -LJO https://raw.githubusercontent.com/supramaxis/scripts/main/customization/.zshrc
+echo -e "[init] \033[0;32mDownloading .zshrc\033[0m"
+
+EOF
+
+# Check the installed version of lsd
+desired_version="1.1.2"
+installed_version=$(dpkg-query -W -f='${Version}' lsd 2>/dev/null)
+
+if [ $? -eq 0 ]; then
+  if dpkg --compare-versions "$installed_version" "ge" "$desired_version"; then
+    log "${GREEN}lsd $installed_version is already installed and is up-to-date.${RESET}"
+    return
+  else
+    log "${GREEN}Upgrading lsd to version $desired_version.${RESET}"
+    wget https://github.com/lsd-rs/lsd/releases/download/v1.1.2/lsd-musl_1.1.2_arm64.deb
+    dpkg -i lsd-musl_1.1.2_arm64.deb
+    rm lsd-musl_1.1.2_arm64.deb
+  fi
+else
+  log "${GREEN}Installing lsd version $desired_version.${RESET}"
+  wget https://github.com/lsd-rs/lsd/releases/download/v1.1.2/lsd-musl_1.1.2_arm64.deb
+  dpkg -i lsd-musl_1.1.2_arm64.deb
+  rm lsd-musl_1.1.2_arm64.deb
+fi
+
+log "${GREEN}Process complete changing to zsh. Please run source ~/.zshrc${RESET}"
+
 ZDOTDIR=~ exec zsh -i
